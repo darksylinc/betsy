04e986aa3982482e63feb38c9e7803824d58c623
Matias N. Goldberg <dark_sylinc@yahoo.com.ar>
2020-05-23 20:10:31 -0300

    Switched to 4 threads per ETC1 block, since subblock1 depends on subblock0 when using color4
    Implemented most of the main loop (WIP)
    
    Implemented final section of ETC1 block (WIP), which encodes the best
    solution into bits & bytes

--- bin/Data/etc1.glsl
+++ bin/Data/etc1.glsl
@@ -14,20 +14,30 @@
 #define FLT_MAX 340282346638528859811704183484516925440.0f
 
 #define cLowQuality 0
+#define cMediumQuality 1
+#define cHighQuality 2
+
+#define TODO_pack_etc1_block_solid_color_constrained
 
 // 2 sets of 16 float3 (rgba8_unorm) for each ETC block
 // We use rgba8_unorm encoding because it's 6kb vs 1.5kb of LDS. The former kills occupancy
 shared uint g_srcPixelsBlock[16 * 2 * 4 * 4];
 
-uniform float p_quality;
+uniform float4 params;
+
+#define p_quality params.x
+#define p_scanDeltaAbsMin params.y
+#define p_scanDeltaAbsMax params.z
 
 // srcTex MUST be in rgba8_unorm otherwise severe dataloss could happen (i.e. do NOT use sRGB)
 // This is due to packing in g_srcPixelsBlock
 uniform sampler2D srcTex;
 
-/*layout( local_size_x = 8,
+layout( rg32ui ) uniform restrict writeonly uimage2D dstTexture;
+
+layout( local_size_x = 4,
 		local_size_y = 4,
-		local_size_z = 4 ) in;*/
+		local_size_z = 4 ) in;
 
 static const float4 g_etc1_inten_tables[cETC1IntenModifierValues] = {
 	float4( -8, -2, 2, 8 ),       float4( -17, -5, 5, 17 ),    float4( -29, -9, 9, 29 ),
@@ -37,22 +47,12 @@ static const float4 g_etc1_inten_tables[cETC1IntenModifierValues] = {
 
 struct PotentialSolution
 {
-	potential_solution() : m_coords(), error( cUINT64_MAX ), m_valid( false ) {}
-
 	float3 rgbIntLS;  // in range [0; limit]
 	uint intenTable;
-	bool bUseColor4;
 
 	// Has 8 selectors, 8-bits each.
 	uint selectors[2];
 	float error;
-
-	void clear()
-	{
-		m_coords.clear();
-		error = FLT_MAX;
-		m_valid = false;
-	}
 };
 
 inline float3 getScaledColor( const float3 rgbInt, const bool bUseColor4 )
@@ -106,7 +106,7 @@ bool evaluate_solution( const float3 blockRgbInt, const bool bUseColor4, const f
 
 		for( uint c = 0u; c < n; ++c )
 		{
-			const float3 srcPixel = unpackUnorm4x8( g_srcPixelsBlock[c] );
+			const float3 srcPixel = unpackUnorm4x8( g_srcPixelsBlock[c + TODO_idx] );
 
 			float best_selector_index = 0;
 			float best_error = calcError( srcPixel, blockColorsInt[0] );
@@ -149,8 +149,7 @@ bool evaluate_solution( const float3 blockRgbInt, const bool bUseColor4, const f
 		}
 	}
 
-	trialSolution.rgbInt = blockRgbInt;
-	trialSolution.bUseColor4 = bUseColor4;
+	trialSolution.rgbIntLS = blockRgbInt;
 
 	bool success = false;
 	if( trialSolution.error < bestSolution.error )
@@ -257,7 +256,7 @@ bool etc1_optimizer_compute( const float scanDeltaAbsMin, const float scanDeltaA
 
 					for( uint r = 0u; r < 8u; ++r )
 					{
-						const uint s = pSelectors[r >> 2u][r & 0x04u] * 255.0f;
+						const uint s = uint( pSelectors[r >> 2u][r & 0x04u] * 255.0f );
 						const int yd = pIntenTable[s];
 						// Compute actual delta being applied to each pixel,
 						// taking into account clamping.
@@ -297,14 +296,24 @@ bool etc1_optimizer_compute( const float scanDeltaAbsMin, const float scanDeltaA
 	}
 }
 
+/// Replaces g_selector_index_to_etc1[cETC1SelectorValues] from original code
+/// const uint8 g_selector_index_to_etc1[cETC1SelectorValues] = { 3, 2, 0, 1 };
+///
+/// Input is in range [0; 4)
+/// Return value is in range [0; 4)
+float selector_index_to_etc1( float idx )
+{
+	return idx < 2.0f ? ( 3.0f - idx ) : idx;
+}
+
 void main()
 {
-	// 8 threads per ETC block. Each pair of threads tries a different mode
-	// combination (flip & useColor4); while we also split subgroups into 2 threads
+	// 4 threads per ETC block. Each pair of threads tries a different mode
+	// combination (flip & useColor4); but we do not split subgroups into 2 threads because
+	// when bUseColor4 = false, subblock1 depends on subblock0
 	//
-	// The 2 subgroups cannot be merged arbitrarily. In Rich Geldrich's implementation the algorithm
-	// would stop if the 1st subgroup error is already higher the combined error of the
-	// best pair of candidates.
+	// In Rich Geldrich's implementation the algorithm would stop if the 1st subgroup error
+	// is already higher the combined error of the best pair of candidates.
 	//
 	// We can't do that because:
 	//	1. There is no "best candidate" because they're all processed in parallel
@@ -312,35 +321,239 @@ void main()
 	//	   from different ETC blocks being processed by the same threadgroup
 	const uint options = gl_LocalInvocationID.x;
 
-	const uint subblockIdx = options & 0x01u;
-	const bool bFlip = ( options & 0x02u ) != 0u;
-	const bool bUseColor4 = ( options & 0x03u ) != 0u;
+	const bool bFlip = ( options & 0x01u ) != 0u;
+	const bool bUseColor4 = ( options & 0x02u ) != 0u;
 
 	const uint2 pixelsToLoadBase = gl_GlobalInvocationID.yz << 1u;
 
-	// We need to load a block of 4x4 pixels from src (16 pixels), and we have 8 threads per block
-	// Have each thread load 2 pixels:
-	//	2x1 2x1
-	//	2x1 2x1
-	//	2x1 2x1
-	//	2x1 2x1
+	// We need to load a block of 4x4 pixels from src (16 pixels), and we have 4 threads per block
+	// Thus each thread gets to load 4 pixels:
+	//	4x1
+	//	4x1
+	//	4x1
+	//	4x1
+	// and distribute it into shared memory
 	uint2 pixelsToLoad = pixelsToLoadBase;
-	pixelsToLoad.x += ( options & 0x01u ) << 1u;  //+= (options % 2) ? 0 : 2;
-	pixelsToLoad.y += options >> 1u;              //+= options / 2;
+	pixelsToLoad.y += options >> 2u;  //+= options / 4;
 
 	const float3 srcPixels0 = OGRE_Load2D( srcTex, pixelsToLoad, 0 ).xyz;
 	const float3 srcPixels1 = OGRE_Load2D( srcTex, pixelsToLoad + uint2( 1u, 0u ), 0 ).xyz;
+	const float3 srcPixels2 = OGRE_Load2D( srcTex, pixelsToLoad + uint2( 2u, 0u ), 0 ).xyz;
+	const float3 srcPixels3 = OGRE_Load2D( srcTex, pixelsToLoad + uint2( 3u, 0u ), 0 ).xyz;
 
-	const uint blockStart = gl_LocalInvocationIndex >> 3u;
+	const uint blockStart = gl_LocalInvocationIndex >> 2u;
 	// Linear (horizontal, aka flip = off)
-	g_srcPixelsBlock[blockStart + ( options << 1u ) + 0u] = packUnorm4x8( srcPixels0 );
-	g_srcPixelsBlock[blockStart + ( options << 1u ) + 1u] = packUnorm4x8( srcPixels1 );
+	const uint subblockStartH = blockStart + ( options << 2u );  // = blockStart + blockThreadId * 4u;
+	g_srcPixelsBlock[subblockStartH + 0u] = packUnorm4x8( float4( srcPixels0, 1.0f ) );
+	g_srcPixelsBlock[subblockStartH + 1u] = packUnorm4x8( float4( srcPixels1, 1.0f ) );
+	g_srcPixelsBlock[subblockStartH + 2u] = packUnorm4x8( float4( srcPixels2, 1.0f ) );
+	g_srcPixelsBlock[subblockStartH + 3u] = packUnorm4x8( float4( srcPixels3, 1.0f ) );
 
 	// Non-linear (vertical, aka flip = on)
-	const uint subblockOffset = subblockIdx << 3u;  //= subblockIdx == 0u ? 0u : 8u
-	const uint subblockStart = blockStart + 16u + subblockOffset + ( options >> 2u );
-	g_srcPixelsBlock[subblockStart + 0u] = packUnorm4x8( srcPixels0 );
-	g_srcPixelsBlock[subblockStart + 4u] = packUnorm4x8( srcPixels1 );
+	const uint subblockOffset0 = 0u;  // subblockIdx = 0
+	const uint subblockStart0 = blockStart + 16u + subblockOffset0 + options;
+	g_srcPixelsBlock[subblockStart0 + 0u] = packUnorm4x8( float4( srcPixels0, 1.0f ) );
+	g_srcPixelsBlock[subblockStart0 + 4u] = packUnorm4x8( float4( srcPixels1, 1.0f ) );
+
+	const uint subblockOffset1 = 8u;  // subblockIdx = 1
+	const uint subblockStart1 = blockStart + 16u + subblockOffset1 + options;
+	g_srcPixelsBlock[subblockStart1 + 0u] = packUnorm4x8( float4( srcPixels2, 1.0f ) );
+	g_srcPixelsBlock[subblockStart1 + 4u] = packUnorm4x8( float4( srcPixels3, 1.0f ) );
+
+	const uint subblockStart = bFlip ? subblockStart1 : subblockStartH;
 
 	__sharedOnlyBarrier;
+
+	PotentialSolution results[2];
+	for( uint i = 0u; i < 2u; ++i )
+	{
+		results[i].error = FLT_MAX;
+		results[i].rgbIntLS = float3( 0, 0, 0 );
+		results[i].intenTable = 0u;
+		results[i].selectors[0] = 0u;
+		results[i].selectors[1] = 0u;
+	}
+
+	TODO_pack_etc1_block_solid_color_constrained;
+
+	// Unfortunately subblockIdx = 1 depends on subblockIdx = 0 when bUseColor4 = false
+	for( uint subblockIdx = 0u; subblockIdx < 2u; ++subblockIdx )
+	{
+		const bool constrainAgainstBaseColor5 = !bUseColor4 && subblockIdx != 0u;
+		const float3 baseColor5 = results[0].rgbIntLS;
+
+		// Have all threads compute average.
+		//
+		// We don't do parallel reduction because it'd consume too
+		// much LDS and we don't have access to __shfl_down
+		float3 avgColour = float3( 0, 0, 0 );
+		for( uint i = 0; i < 8u; ++i )
+		{
+			const float3 srcPixel = unpackUnorm4x8( g_srcPixelsBlock[i + TODO_idx] );
+			avgColour += srcPixel;
+		}
+		avgColour *= 1.0f / 8.0f;
+
+		const float limit = bUseColor4 ? 15 : 31;
+		const float3 avgColourLS = clamp( round( avgColour * limit * ( 1.0f / 255.0f ) ), 0, limit );
+
+		const bool bResult = etc1_optimizer_compute( p_scanDeltaAbsMin, p_scanDeltaAbsMax, avgColour,
+													 avgColourLS, bUseColor4, baseColor5,
+													 constrainAgainstBaseColor5, results[subblockIdx] );
+
+		if( !allInvocationsARB( bResult ) )
+			break;
+
+		if( p_quality >= cMediumQuality )
+		{
+			const float refinement_error_thresh0 = 3000;
+			const float refinement_error_thresh1 = 6000;
+			if( results[subblockIdx].error > refinement_error_thresh0 )
+			{
+				float scanDeltaAbsMin, scanDeltaAbsMax;
+				if( p_quality == cMediumQuality )
+				{
+					scanDeltaAbsMin = 2;
+					scanDeltaAbsMax = 3;
+				}
+				else
+				{
+					static const int s_scan_delta_5_to_5[] = { -5, 5 };
+					static const int s_scan_delta_5_to_8[] = { -8, -7, -6, -5, 5, 6, 7, 8 };
+					scanDeltaAbsMin = 5;
+					if( results[subblockIdx].error > refinement_error_thresh1 )
+						scanDeltaAbsMax = 8;
+					else
+						scanDeltaAbsMax = 5;
+				}
+
+				const bool refinerResult = etc1_optimizer_compute(
+					p_scanDeltaAbsMin, p_scanDeltaAbsMax, avgColour, avgColourLS, bUseColor4, baseColor5,
+					constrainAgainstBaseColor5, results[subblockIdx] );
+
+				if( !allInvocationsARB( refinerResult ) )
+					break;
+			}
+
+			if( results[2].m_error < results[subblockIdx].m_error )
+				results[subblockIdx] = results[2];
+		}
+
+		TODO_end;
+	}
+
+	const bool bBestFlip = TODO;
+	const bool bBestUseColor4 = TODO;
+
+	float4 bytes;
+	if( bUseColor4 )
+	{
+		bytes.xyz = results[1].rgbIntLS + results[0].rgbIntLS * 16.0f;
+	}
+	else
+	{
+		float3 delta = results[1].rgbIntLS - results[0].rgbIntLS;
+		if( delta.x < 0.0f )
+			delta.x += 8.0f;
+		if( delta.y < 0.0f )
+			delta.y += 8.0f;
+		if( delta.z < 0.0f )
+			delta.z += 8.0f;
+
+		bytes.xyz = delta + results[0].rgbIntLS * 8.0f;
+	}
+
+	bytes.w = ( results[1].intenTable * 4.0f ) + ( results[0].intenTable * 32.0f ) +
+			  ( bBestUseColor4 ? 0.0f : 2.0f ) + ( bBestFlip ? 1.0f : 0.0f );
+
+	uint2 outputBytes;
+	outputBytes.x = packUnorm4x8( bytes * ( 1.0f / 255.0f ) );
+
+	float selector0 = 0, selector1 = 0;
+	if( bBestFlip )
+	{
+		// flipped:
+		// { 0, 0 }, { 1, 0 }, { 2, 0 }, { 3, 0 },
+		// { 0, 1 }, { 1, 1 }, { 2, 1 }, { 3, 1 }
+		//
+		// { 0, 2 }, { 1, 2 }, { 2, 2 }, { 3, 2 },
+		// { 0, 3 }, { 1, 3 }, { 2, 3 }, { 3, 3 }
+		float4 pSelectors00 = unpackUnorm4x8( results[0].selectors[0] ) * 255.0f;
+		float4 pSelectors01 = unpackUnorm4x8( results[0].selectors[1] ) * 255.0f;
+		float4 pSelectors10 = unpackUnorm4x8( results[1].selectors[0] ) * 255.0f;
+		float4 pSelectors11 = unpackUnorm4x8( results[1].selectors[1] ) * 255.0f;
+		for( uint x = 4u; x--; )
+		{
+			float b, bHalf;
+			b = selector_index_to_etc1( pSelectors11[x] );
+			bHalf = floor( b * 0.5f );
+			selector0 = ( selector0 * 2.0f ) + ( b - bHalf * 2.0f );
+			selector1 = ( selector1 * 2.0f ) + ( bHalf );
+
+			b = selector_index_to_etc1( pSelectors10[x] );
+			bHalf = floor( b * 0.5f );
+			selector0 = ( selector0 * 2.0f ) + ( b - bHalf * 2.0f );
+			selector1 = ( selector1 * 2.0f ) + ( bHalf );
+
+			b = selector_index_to_etc1( pSelectors01[x] );
+			bHalf = floor( b * 0.5f );
+			selector0 = ( selector0 * 2.0f ) + ( b - bHalf * 2.0f );
+			selector1 = ( selector1 * 2.0f ) + ( bHalf );
+
+			b = selector_index_to_etc1( pSelectors00[x] );
+			bHalf = floor( b * 0.5f );
+			selector0 = ( selector0 * 2.0f ) + ( b - bHalf * 2.0f );
+			selector1 = ( selector1 * 2.0f ) + ( bHalf );
+		}
+	}
+	else
+	{
+		// non-flipped:
+		// { 0, 0 }, { 0, 1 }, { 0, 2 }, { 0, 3 },
+		// { 1, 0 }, { 1, 1 }, { 1, 2 }, { 1, 3 }
+		//
+		// { 2, 0 }, { 2, 1 }, { 2, 2 }, { 2, 3 },
+		// { 3, 0 }, { 3, 1 }, { 3, 2 }, { 3, 3 }
+		for( uint subblockIdx = 2u; subblockIdx--; )
+		{
+			for( uint i = 2u; i--; )
+			{
+				float4 pSelectors = unpackUnorm4x8( results[subblockIdx].selectors[i] ) * 255.0f;
+
+				float b, bHalf;
+				b = selector_index_to_etc1( pSelectors[3] );
+				bHalf = floor( b * 0.5f );
+				selector0 = ( selector0 * 2.0f ) + ( b - bHalf * 2.0f );
+				selector1 = ( selector1 * 2.0f ) + ( bHalf );
+
+				b = selector_index_to_etc1( pSelectors[2] );
+				bHalf = floor( b * 0.5f );
+				selector0 = ( selector0 * 2.0f ) + ( b - bHalf * 2.0f );
+				selector1 = ( selector1 * 2.0f ) + ( bHalf );
+
+				b = selector_index_to_etc1( pSelectors[1] );
+				bHalf = floor( b * 0.5f );
+				selector0 = ( selector0 * 2.0f ) + ( b - bHalf * 2.0f );
+				selector1 = ( selector1 * 2.0f ) + ( bHalf );
+
+				b = selector_index_to_etc1( pSelectors[0] );
+				bHalf = floor( b * 0.5f );
+				selector0 = ( selector0 * 2.0f ) + ( b - bHalf * 2.0f );
+				selector1 = ( selector1 * 2.0f ) + ( bHalf );
+			}
+		}
+	}
+
+#ifdef CHECKING_ORDER
+	bytes.x = selector1 / 256.0f;
+	bytes.y = selector1 - floor( selector1 / 256.0f ) * 256.0f;
+	bytes.z = selector0 / 256.0f;
+	bytes.w = selector0 - floor( selector0 / 256.0f ) * 256.0f;
+	outputBytes.y = packUnorm4x8( bytes * ( 1.0f / 255.0f ) );
+#else
+	outputBytes.y = packUnorm2x16( float2( selector1, selector0 ) * ( 1.0f / 65535.0f ) );
+#endif
+
+	uint2 dstUV = gl_GlobalInvocationID.yz >> 2u;
+	imageStore( dstTexture, int2( dstUV ), outputBytes );
 }
